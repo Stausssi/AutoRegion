ff6785b1b0f81768a2309eb97aa8adf23aef6cb9
Stausssi <stauss.simon.01@gmail.com>
2022-09-22 21:26:26 +0200

    changed version

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,6 +1,6 @@
 echo off
 set version=%1
 set serverPath="C:\Users\Simon\Documents\Minecraft Server\%version%"
-copy target\autoregion-1.0.jar %serverPath%\plugins\
+copy target\autoregion-0.6.2.jar %serverPath%\plugins\
 cd %serverPath%
 .\start.bat
