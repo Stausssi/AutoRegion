d6bd287f0fbc8c9d79fbbe7c2172930f05208a29
Simon <stauss.simon.01@gmail.com>
2022-12-24 01:06:23 +0100

    renamed player to sender

--- src/main/java/io/stausssi/plugins/autoregion/MessageHandler.java
+++ src/main/java/io/stausssi/plugins/autoregion/MessageHandler.java
@@ -28,7 +28,7 @@ public class MessageHandler {
      */
     public void init(String pluginName) {
         sysPrefix = String.format("[%s]", pluginName);
-        prefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("prefix", "PREFIX_MISSING"));
+        prefix = convertColors(configHandler.getString("prefix"));
         wideChatPrefix = "§8-------- " + prefix + "--------\n";
 
         initialized = true;
@@ -79,75 +79,75 @@ public class MessageHandler {
     }
 
     /**
-     * Sends a colored message retrieved from the config to the given player. Mostly used as a response to a command.
+     * Sends a colored message retrieved from the config to the given sender. Mostly used as a response to a command.
      * Will automatically prefix the message.
      *
-     * @param player     The player to send the message to.
+     * @param sender     The sender to send the message to.
      * @param messageKey The key of the message in the plugin config. The messages prefixed will be prepended
      *                   automatically.
      */
-    public void sendMessage(CommandSender player, String messageKey) {
-        sendMessage(player, messageKey, true);
+    public void sendMessage(CommandSender sender, String messageKey) {
+        sendMessage(sender, messageKey, true);
     }
 
 
     /**
-     * Sends a message retrieved from the config to the given player. Mostly used as a response to a command. Will
+     * Sends a message retrieved from the config to the given sender. Mostly used as a response to a command. Will
      * automatically prefix the message.
      *
-     * @param player     The player to send the message to.
+     * @param sender     The sender to send the message to.
      * @param messageKey The key of the message in the plugin config. The messages prefixed will be prepended
      *                   automatically.
      * @param color      Whether the color codes in the message should be converted.
      */
-    public void sendMessage(CommandSender player, String messageKey, boolean color) {
-        sendMessage(player, messageKey, color, null);
+    public void sendMessage(CommandSender sender, String messageKey, boolean color) {
+        sendMessage(sender, messageKey, color, null);
     }
 
     /**
-     * Sends a colored message from the config to the player. Will also replace placeholders with their given values.
+     * Sends a colored message from the config to the sender. Will also replace placeholders with their given values.
      *
-     * @param player       The player to send the message to.
+     * @param sender       The sender to send the message to.
      * @param messageKey   The key of the message in the plugin config. The messages prefixed will be prepended
      *                     automatically.
      * @param replacements The keys contain the placeholders which will be replaced with the corresponding values.
      */
-    public void sendMessage(CommandSender player, String messageKey, Map<String, String> replacements) {
-        sendMessage(player, messageKey, true, replacements);
+    public void sendMessage(CommandSender sender, String messageKey, Map<String, String> replacements) {
+        sendMessage(sender, messageKey, true, replacements);
     }
 
     /**
-     * Sends a message from the config to the player. Will also replace placeholders with their given values.
+     * Sends a message from the config to the sender. Will also replace placeholders with their given values.
      *
-     * @param player       The player to send the message to.
+     * @param sender       The sender to send the message to.
      * @param messageKey   The key of the message in the plugin config. The messages prefixed will be prepended
      *                     automatically.
      * @param color        Whether the color codes in the message should be converted.
      * @param replacements The keys contain the placeholders which will be replaced with the corresponding values.
      */
-    public void sendMessage(CommandSender player, String messageKey, boolean color, Map<String, String> replacements) {
-        sendMessage(player, messageKey, color, replacements, true);
+    public void sendMessage(CommandSender sender, String messageKey, boolean color, Map<String, String> replacements) {
+        sendMessage(sender, messageKey, color, replacements, true);
     }
 
     /**
-     * Sends a message retrieved from the config to the given player. Mostly used as a response to a command. Will
+     * Sends a message retrieved from the config to the given sender. Mostly used as a response to a command. Will
      * automatically prefix the message.
      *
-     * @param player       The player to send the message to.
+     * @param sender       The sender to send the message to.
      * @param message      Either the key of the message or the message text.
      * @param color        Whether the color codes in the message should be converted.
      * @param replacements The keys contain the placeholders which will be replaced with the corresponding values.
      * @param fromConfig   True, if the given message is a key and should be retrieved from the config. If false, just the
      *                     given message will be sent.
      */
-    public void sendMessage(CommandSender player, String message, boolean color, Map<String, String> replacements, boolean fromConfig) {
-        sendMessage(player, message, color, replacements, fromConfig, true);
+    public void sendMessage(CommandSender sender, String message, boolean color, Map<String, String> replacements, boolean fromConfig) {
+        sendMessage(sender, message, color, replacements, fromConfig, true);
     }
 
     /**
-     * Sends a message retrieved to the given player. Mostly used as a response to a command.
+     * Sends a message retrieved to the given sender. Mostly used as a response to a command.
      *
-     * @param player       The player to send the message to.
+     * @param sender       The sender to send the message to.
      * @param message      Either the key of the message or the message text.
      * @param color        Whether the color codes in the message should be converted.
      * @param replacements The keys contain the placeholders which will be replaced with the corresponding values.
@@ -155,14 +155,14 @@ public class MessageHandler {
      *                     given message will be sent.
      * @param prefix       Whether the message should be prefixed.
      */
-    public void sendMessage(CommandSender player, String message, boolean color, Map<String, String> replacements, boolean fromConfig, boolean prefix) {
-        sendMessage(player, message, color, replacements, fromConfig, prefix, false);
+    public void sendMessage(CommandSender sender, String message, boolean color, Map<String, String> replacements, boolean fromConfig, boolean prefix) {
+        sendMessage(sender, message, color, replacements, fromConfig, prefix, false);
     }
 
     /**
-     * Sends a message retrieved to the given player. Mostly used as a response to a command.
+     * Sends a message retrieved to the given sender. Mostly used as a response to a command.
      *
-     * @param player       The player to send the message to.
+     * @param sender       The sender to send the message to.
      * @param message      Either the key of the message or the message text.
      * @param color        Whether the color codes in the message should be converted.
      * @param replacements The keys contain the placeholders which will be replaced with the corresponding values.
@@ -171,7 +171,7 @@ public class MessageHandler {
      * @param prefix       Whether the message should be prefixed.
      * @param widePrefix   Whether the wide prefix spanning an entire chat line should be used.
      */
-    public void sendMessage(CommandSender player, String message, boolean color, Map<String, String> replacements, boolean fromConfig, boolean prefix, boolean widePrefix) {
+    public void sendMessage(CommandSender sender, String message, boolean color, Map<String, String> replacements, boolean fromConfig, boolean prefix, boolean widePrefix) {
         // Force the class to be initialized if prefixes are used
         if ((prefix || widePrefix) && !checkInit()) {
             return;
@@ -199,18 +199,23 @@ public class MessageHandler {
             }
         }
 
-        player.sendMessage(message);
+        sender.sendMessage(message);
     }
 
     /**
-     * Informs the player that they don't have sufficient privilege to execute the requested action.
+     * Informs the sender that they don't have sufficient privilege to execute the requested action.
      *
-     * @param sender The player to inform.
+     * @param sender The sender to inform.
      */
     public void noPermission(CommandSender sender) {
         sendMessage(sender, "noPermission");
     }
 
+    /**
+     * Send all available commands with a small description to the given sender.
+     *
+     * @param sender The sender to send the help message to.
+     */
     public void sendHelp(CommandSender sender) {
         sendMessage(
                 sender,
@@ -218,11 +223,11 @@ public class MessageHandler {
                         + "§6/autoregion updates [enable/disable] §7- Enables/Disables automatic updating\n"
                         + "§6/autoregion list §7- Gives you a list of all RegionCreators\n"
                         + "§6/autoregion help §7- Displays a list with all commands\n"
-                        + "§6/autoregion add [BlockName] [Radius] §7- Adds a RegionCreator to the config\n"
-                        + "§6/autoregion remove [BlockName] §7- Removes a RegionCreator from the config\n"
-                        + "§6/autoregion give [BlockName] [Player] §7- Gives a player a RegionCreator to create a region\n"
+                        + "§6/autoregion add [BLOCK_IDENTIFIER] [radius] §7- Adds a RegionCreator to the config\n"
+                        + "§6/autoregion remove [BLOCK_IDENTIFIER] §7- Removes a RegionCreator from the config\n"
+                        + "§6/autoregion give [BLOCK_IDENTIFIER] [player] §7- Gives a player a RegionCreator to create a region\n"
                         + "\n"
-                        + "§4[BlockName] has to be a valid RegionCreator, for instance: 'DIAMOND_ORE'",
+                        + "§4[BLOCK_IDENTIFIER] has to be a valid RegionCreator, for instance: 'DIAMOND_ORE'",
                 true, null, false, false, true
         );
     }
